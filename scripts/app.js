dc5c0eaaf1ae1294909608e61f76f6684771c357
River Hermsen <riverhermsen@hotmail.nl>
2019-09-04 12:12:04 +0200

    Added major changes

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+var options = {
+    strings: ['Hoi! Ik ben River<span class="theme-color">.</span>', 'Ik ben een Software Engineer student<span class="theme-color">.</span>'],
+    typeSpeed: 80,
+    backDelay: 1000,
+    loop: true
+}
+
+var typed = new Typed("#typed-strings", options);
+
+function initMap() {
+    // Styles a map in night mode.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 40.674, lng: -73.945 },
+        zoom: 12,
+    });
+}
+
