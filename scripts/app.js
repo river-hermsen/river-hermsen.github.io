b13ce420b8cb0bd78fd114ed51f58fbb7a79aa62
River Hermsen <riverhermsen@hotmail.nl>
2019-09-04 12:19:46 +0200

    Added firebase and updated 404

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,6 @@ var options = {
 var typed = new Typed("#typed-strings", options);
 
 function initMap() {
-    // Styles a map in night mode.
     var map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 40.674, lng: -73.945 },
         zoom: 12,
